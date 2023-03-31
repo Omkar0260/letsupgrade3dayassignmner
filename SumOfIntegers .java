f7fa3c947c26fb3a70ec295f178f0b4b92258985
Omkar0260 <76987405+Omkar0260@users.noreply.github.com>
2023-03-31 16:34:33 +0530

    Add files via upload

---
+++ SumOfIntegers .java
@@ -0,0 +1,12 @@
+public class SumOfIntegers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int result = sum(a, b);
+        System.out.println("The sum of " + a + " and " + b + " is " + result);
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
